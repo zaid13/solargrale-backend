1293fb5dbe302e63e36f77f125a22f551fc850e2
zaid bin saeed <47746351+zaid13@users.noreply.github.com>
2024-03-25 15:16:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://127.0.0.1:8086",
-    "https://solar-glare-web.vercel.app"
+    "https://solar-glare-web.vercel.app",
+    "http://solar-glare-web.vercel.app"
 ]
 
 # -*- coding: utf-8 -*-
